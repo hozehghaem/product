f751eb571d0a6d397f73fbb5f046f1422c1240f0
amir <amirhpz2000@gmail.com>
2024-12-23 19:21:14 +0330

    v1.2.36 add single kanoon

--- resources/views/Site/classcenter.blade.php
+++ resources/views/Site/classcenter.blade.php
@@ -57,37 +57,55 @@
 
         <!-- کارت‌های فعالیت‌ها -->
         <h3 class="mb-4">فعالیت‌ها</h3>
+
         <div class="row">
             <div class="col-md-4">
-                <div class="card shadow-sm mb-4 h-100">
-                    <img src="{{asset('/site/img/conference.jpg')}}" class="img-fluid" alt="">
-                    <div class="card-body justify-content-between">
-                        <h5 class="card-title">آموزش حفظ قرآن</h5>
-                        <p class="card-text">کلاس‌های پیشرفته حفظ قرآن ویژه علاقه‌مندان برگزار می‌شود.</p>
-                        <span class="activity-tag">کانون نور الثقلین</span>
-                    </div>
-                </div>
+                @foreach($posts as $post)
+                    @if($post->posttype == 9)
+                        <div class="col-md-4 py-4 z-3">
+                            <div class="post-card">
+                                <img src="{{ asset($post->image) }}" alt="{{ $post->title }}">
+                                <div class="post-card-body">
+                                    <h4 class="post-title">{{ $post->title }}</h4>
+                                    <p class="post-description">{{ Str::limit(strip_tags($post->description), 40, '...') }}
+                                    </p>
+                                    <a href="{{ url('حوزه-علمیه-خواهران/معاونت-پژوهش/نشست-ها/'.$post->slug) }}"
+                                       class="btn btn-primary btn-sm">مشاهده بیشتر</a>
+{{--                                    <span class="activity-tag">کانون نور الثقلین</span>--}}
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+{{--                <div class="card shadow-sm mb-4 h-100">--}}
+{{--                    <img src="{{asset('/site/img/conference.jpg')}}" class="img-fluid" alt="">--}}
+{{--                    <div class="card-body justify-content-between">--}}
+{{--                        <h5 class="card-title">آموزش حفظ قرآن</h5>--}}
+{{--                        <p class="card-text">کلاس‌های پیشرفته حفظ قرآن ویژه علاقه‌مندان برگزار می‌شود.</p>--}}
+{{--                        <span class="activity-tag">کانون نور الثقلین</span>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
             </div>
-            <div class="col-md-4">
-                <div class="card shadow-sm mb-4 h-100">
-                    <img src="{{asset('/site/img/conference.jpg')}}" class="img-fluid" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">مشاوره خانواده</h5>
-                        <p class="card-text">جلسات مشاوره خانوادگی با رویکرد دینی و علمی.</p>
-                        <span class="activity-tag">کانون زن و خانواده</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card shadow-sm mb-4 h-100">
-                    <img src="{{asset('/site/img/conference.jpg')}}" class="img-fluid" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">تربیت مبلغ</h5>
-                        <p class="card-text">دوره‌های ویژه تربیت مبلغ معارف اسلامی.</p>
-                        <span class="activity-tag">کانون دانش‌آموختگان</span>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="col-md-4">--}}
+{{--                <div class="card shadow-sm mb-4 h-100">--}}
+{{--                    <img src="{{asset('/site/img/conference.jpg')}}" class="img-fluid" alt="">--}}
+{{--                    <div class="card-body">--}}
+{{--                        <h5 class="card-title">مشاوره خانواده</h5>--}}
+{{--                        <p class="card-text">جلسات مشاوره خانوادگی با رویکرد دینی و علمی.</p>--}}
+{{--                        <span class="activity-tag">کانون زن و خانواده</span>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="col-md-4">--}}
+{{--                <div class="card shadow-sm mb-4 h-100">--}}
+{{--                    <img src="{{asset('/site/img/conference.jpg')}}" class="img-fluid" alt="">--}}
+{{--                    <div class="card-body">--}}
+{{--                        <h5 class="card-title">تربیت مبلغ</h5>--}}
+{{--                        <p class="card-text">دوره‌های ویژه تربیت مبلغ معارف اسلامی.</p>--}}
+{{--                        <span class="activity-tag">کانون دانش‌آموختگان</span>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 @endsection
