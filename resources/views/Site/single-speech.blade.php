0e677677b7cc16b924c3c556ef417beaf762556d
amir <amirhpz2000@gmail.com>
2025-02-11 19:54:51 +0330

    v1.2.65 add speech

---
+++ resources/views/Site/single-speech.blade.php
@@ -0,0 +1,128 @@
+@extends('master')
+@section('style')
+@endsection
+@section('main')
+
+    <style>
+        .video-container {
+            position: relative;
+            width: 100%;
+            max-width: 640px;
+            margin: 0 auto;
+        }
+        .video-cover {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: url('{{ asset('site/img/blog-image/1.jpg') }}') no-repeat center center;
+            background-size: cover;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .video-cover::before {
+            background:url('{{ asset('site/img/blog-image/1.jpg') }}') no-repeat center center;
+            content: "▶";
+            font-size: 48px;
+            color: black;
+        }
+        video {
+            display: none;
+            width: 100%;
+        }
+        video.playing {
+            display: block;
+            max-width: 100%;
+            object-fit: fill;
+        }
+    </style>
+    <!-- Start Page Title Area -->
+    <div class="page-title-area">
+        <div class="container">
+            <div class="page-title-content">
+                <h2>{{$posts->title}}</h2>
+            </div>
+        </div>
+    </div>
+    <!-- End Page Title Area -->
+    <!-- Service details -->
+    <div class="service-details">
+        <div class="container my-5" style="border: 1px solid rgba(69,69,69,0.2); border-radius: 16px; box-shadow: 0 4px 32px 0 rgba(0,0,0,0.1);">
+            <div class="container mt-5">
+                {{--                <div class="row " style="justify-content: center">--}}
+                {{--                    <div class="col-md-8" >--}}
+                {{--                        <div class="video-box">--}}
+                {{--                            <img src="{{asset('/site/img/blog-image/1.jpg')}}" class="main-image" alt="image">--}}
+                {{--                            <a href="{{asset('/site/video/1.mp4')}}" class="video-btn popup-youtube">--}}
+                {{--                                <i class="bx bx-play"></i>--}}
+                {{--                            </a>--}}
+                {{--                        </div>--}}
+                {{--                    </div>--}}
+                {{--                </div>--}}
+                <div class="card-image text-center m-3">
+
+                    <img src="{{asset($posts->image)}}" alt="{{$posts->title}}" class="single-image">
+
+                    @if($posts->file)
+                        <video controls preload="metadata" poster="{{asset($posts->image)}}" id="player"
+                               style="width: 100%">
+                            <source src="{{asset($posts->file)}}" type="video/mp4"/>
+                        </video>
+                    @elseif($posts->aparat)
+                        {!! $posts->aparat !!}
+                    @endif
+                </div>
+                <div class="service-details-content">
+
+                    <p>{!! $posts->description !!}</p>
+
+                    {{--                <div class="service-details-info" style="margin-bottom: 100px">--}}
+                    {{--                    <div class="single-info-box">--}}
+                    {{--                        <h4>سخنران</h4>--}}
+                    {{--                        <span>آیت الله هاشمی اولیا</span>--}}
+                    {{--                    </div>--}}
+
+                    {{--                    <div class="single-info-box">--}}
+                    {{--                        <h4>دسته بندی</h4>--}}
+                    {{--                        <span>خانواده</span>--}}
+                    {{--                    </div>--}}
+
+                    {{--                    <div class="single-info-box">--}}
+                    {{--                        <h4>تاریخ</h4>--}}
+                    {{--                        <span>28 دی 1398</span>--}}
+                    {{--                    </div>--}}
+
+                    {{--                    <div class="single-info-box">--}}
+                    {{--                        <h4>اشتراک گذاری</h4>--}}
+                    {{--                        <ul class="social">--}}
+                    {{--                            <li>--}}
+                    {{--                                <a target="_blank" href="#">--}}
+                    {{--                                    <i class='bx bxl-linkedin'></i>--}}
+                    {{--                                </a>--}}
+                    {{--                            </li>--}}
+                    {{--                            <li>--}}
+                    {{--                                <a target="_blank" href="#">--}}
+                    {{--                                    <i class='bx bxl-twitter'></i>--}}
+                    {{--                                </a>--}}
+                    {{--                            </li>--}}
+                    {{--                            <li>--}}
+                    {{--                                <a target="_blank" href="#">--}}
+                    {{--                                    <i class='bx bxl-facebook'></i>--}}
+                    {{--                                </a>--}}
+                    {{--                            </li>--}}
+                    {{--                            <li>--}}
+                    {{--                                <a target="_blank" href="#">--}}
+                    {{--                                    <i class='bx bxl-instagram'></i>--}}
+                    {{--                                </a>--}}
+                    {{--                            </li>--}}
+                    {{--                        </ul>--}}
+                    {{--                    </div>--}}
+                    {{--                </div>--}}
+                </div>
+            </div>
+        </div>
+        <!-- End Service details -->
+@endsection
--- resources/views/Site/speech.blade.php
+++ resources/views/Site/speech.blade.php
@@ -7,87 +7,151 @@
     <div class="page-title-area">
         <div class="container">
             <div class="page-title-content">
-                <h2>سخنرانی آیت الله هاشمی اولیا</h2>
-                <p>اخلاق و صبر</p>
+                <h2>سلسله نشست ها</h2>
+                <p>اخبار نشست های ما</p>
             </div>
         </div>
     </div>
     <!-- End Page Title Area -->
 
-    <!-- Service details -->
-    <div class="service-details ptb-100">
+    <!-- Start Blog Area -->
+    <section class="blog-area ptb-100">
         <div class="container">
-            <div class="container mt-5">
-                <div class="row " style="justify-content: center">
-                    <div class="col-md-8" >
-                        <img src="{{asset('/site/img/media/mahdia1.jpg')}}" class="mx-auto d-block" alt="image">
-                    </div>
-                </div>
-                <div class="service-details-content">
-                    <div class="row pt-5">
-                        <div class="col-12 text-center">
-                            <audio controls>
-                                <source src="{{asset('/site/audio/sample-9s.mp3')}}" type="audio/mpeg">
-                                Your browser does not support the audio element.
-                            </audio>
-                        </div>
-                    </div>
-                    <h3 class="pb-4" style="margin-top: 40px">موضوعات اصلی</h3>
+            <div class="row">
+                <div class="col-lg-4 col-md-12">
+                    <aside class="widget-area" id="secondary">
+                        <section class="widget widget_search">
+                            <form class="search-form">
+                                <label>
+                                    <span class="screen-reader-text">جستجو کنید:</span>
+                                    <input type="search" class="search-field" placeholder="جستجو ...">
+                                </label>
+                                <button type="submit"><i class='bx bx-search-alt'></i></button>
+                            </form>
+                        </section>
 
-                    <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                        <section class="widget widget_spacle_posts_thumb">
+                            <h3 class="widget-title">نشست های محبوب</h3>
 
-                    <h4 class="pb-2">ابعاد سخنرانی</h4>
-                    <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                            <article class="item">
+                                <a href="#" class="thumb">
+                                    <span class="fullimage cover bg1" role="img"></span>
+                                </a>
+                                <div class="info">
+                                    <time datetime="2019-06-30">10 دی 1398</time>
+                                    <h4 class="title usmall"><a href="#">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر
+                                            می گیرد.</a></h4>
+                                </div>
 
-                    <h4>نتایج جلسه</h4>
-                    <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                                <div class="clear"></div>
+                            </article>
 
-                    <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                            <article class="item">
+                                <a href="#" class="thumb">
+                                    <span class="fullimage cover bg2" role="img"></span>
+                                </a>
+                                <div class="info">
+                                    <time datetime="2019-06-30">10 دی 1398</time>
+                                    <h4 class="title usmall"><a href="#">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر
+                                            می گیرد.</a></h4>
+                                </div>
 
-                    <div class="service-details-info" style="margin-bottom: 100px">
-                        <div class="single-info-box">
-                            <h4>سخنران</h4>
-                            <span>آیت الله هاشمی اولیا</span>
-                        </div>
+                                <div class="clear"></div>
+                            </article>
 
-                        <div class="single-info-box">
-                            <h4>دسته بندی</h4>
-                            <span>خانواده</span>
-                        </div>
+                            <article class="item">
+                                <a href="#" class="thumb">
+                                    <span class="fullimage cover bg3" role="img"></span>
+                                </a>
+                                <div class="info">
+                                    <time datetime="2019-06-30">10 دی 1398</time>
+                                    <h4 class="title usmall"><a href="#">لورم ایپسوم به سادگی ساختار چاپ و متن را در بر
+                                            می گیرد.</a></h4>
+                                </div>
 
-                        <div class="single-info-box">
-                            <h4>تاریخ</h4>
-                            <span>28 دی 1398</span>
-                        </div>
+                                <div class="clear"></div>
+                            </article>
+                        </section>
 
-                        <div class="single-info-box">
-                            <h4>اشتراک گذاری</h4>
-                            <ul class="social">
-                                <li>
-                                    <a target="_blank" href="#">
-                                        <i class='bx bxl-linkedin'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a target="_blank" href="#">
-                                        <i class='bx bxl-twitter'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a target="_blank" href="#">
-                                        <i class='bx bxl-facebook'></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a target="_blank" href="#">
-                                        <i class='bx bxl-instagram'></i>
-                                    </a>
-                                </li>
+                        <section class="widget widget_categories">
+                            <h3 class="widget-title">دسته بندی ها</h3>
+
+                            <ul>
+                                <li><a href="#">مهدویت</a></li>
+                                <li><a href="#">امام شناسی</a></li>
+                                <li><a href="#">فقه و تدین</a></li>
+                                <li><a href="#">خانواده</a></li>
+                                <li><a href="#">دسته بندی نشده</a></li>
                             </ul>
+                        </section>
+
+                        <section class="widget widget_archive">
+                            <h3 class="widget-title">بایگانی ها</h3>
+
+                            <ul>
+                                <li><a href="#">مهر 1398</a></li>
+                                <li><a href="#">آبان 1398</a></li>
+                                <li><a href="#">آذر 1398</a></li>
+                            </ul>
+                        </section>
+
+                    </aside>
+                </div>
+                <div class="col-lg-8 col-md-12">
+                    <div class="row">
+                        @foreach($posts as $post)
+                            @if($post->posttype == 5)
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="single-blog-post">
+                                        <div class="post-image">
+                                            <a href="{{url('حوزه-علمیه-خواهران/سخنرانی-مذهبی/سخنرانی-ها/'.$post->slug)}}"><img
+                                                    src="{{asset($post->image)}}" alt="image"></a>
+
+                                            <div class="date"><i
+                                                    class='bx bx-calendar'></i>{{jdate($post->update_at)->format('y/m/d')}}
+                                            </div>
+                                        </div>
+
+                                        <div class="post-content">
+                                            <h3>
+                                                <a href="{{url('حوزه-علمیه-خواهران/سخنرانی-مذهبی/سخنرانی-ها/'.$post->slug)}}">{{$post->title}}</a>
+                                            </h3>
+
+                                            <div class="post-info">
+                                                <div class="post-by">
+                                                    <img src="{{asset($post->image)}}" alt="{{$post->title}}">
+
+                                                    {{--                                            <h6>فاطمه محمدی</h6>--}}
+                                                </div>
+
+                                                <div class="details-btn">
+                                                    <a href="{{url('حوزه-علمیه-خواهران/سخنرانی-مذهبی/سخنرانی-ها/'.$post->slug)}}"><i
+                                                            class="bx bx-left-arrow-alt"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+
+                        <div class="col-lg-12 col-md-12">
+                            <div class="pagination-area">
+                                {{--                                <a href="#" class="prev page-numbers"><i class='bx bxs-arrow-to-right'></i></a>--}}
+                                {{--                                <a href="#" class="page-numbers">1</a>--}}
+                                {{--                                <span class="page-numbers current" aria-current="page">2</span>--}}
+                                {{--                                <a href="#" class="page-numbers">3</a>--}}
+                                {{--                                <a href="#" class="next page-numbers"><i class='bx bxs-arrow-to-left'></i></a>--}}
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
-        <!-- End Service details -->
+    </section>
+    <!-- End Blog Area -->
+
 @endsection
+
+
