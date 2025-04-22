93b49bd99f31d3cebd26d04ac3c087b53c17dda6
hosseindbk <hosseindbk@yahoo.com>
2025-04-22 19:06:55 +0330

    v1.2.74 edit type_user

--- app/Http/Controllers/Admin/CustomertypeController.php
+++ app/Http/Controllers/Admin/CustomertypeController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Dashboard\CustomertypeRequest;
 use App\Models\Dashboard\Customertype;
 use App\Models\Dashboard\Menu_panel;
 use App\Models\Dashboard\Submenu_panel;
+use App\Models\TypeUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -19,19 +20,19 @@ class CustomertypeController extends Controller
     public function index(Request $request)
     {
         $thispage       = [
-            'title'         => 'مدیریت نوع مشتریان',
-            'list_title'    => 'لیست نوع مشتریان',
-            'add_title'     => 'افزودن نوع مشتریان',
-            'create_title'  => 'ایجاد نوع مشتریان',
-            'edit_title'    => 'ویرایش نوع مشتریان',
-            'enter_title'   => 'ورود اطلاعات نوع مشتریان',
+            'title'         => 'مدیریت نوع کاربران',
+            'list_title'    => 'لیست نوع کاربران',
+            'add_title'     => 'افزودن نوع کاربران',
+            'create_title'  => 'ایجاد نوع کاربران',
+            'edit_title'    => 'ویرایش نوع کاربران',
+            'enter_title'   => 'ورود اطلاعات نوع کاربران',
         ];
         $customertypes  =   Customertype::all();
         $menupanels     =   Menu_panel::whereStatus(4)->get();
         $submenupanels  =   Submenu_panel::whereStatus(4)->get();
 
         if ($request->ajax()) {
-            $data = DB::table('customertypes')
+            $data = DB::table('type_users')
                 //->join('menus' , 'slides.menu_id' , '=' , 'menus.id')
                 //->select('menus.title as menu' , 'slides.id', 'slides.title1', 'slides.title2', 'slides.title3', 'slides.status', 'slides.file_link')
                 ->get();
@@ -41,7 +42,10 @@ class CustomertypeController extends Controller
                 ->addColumn('id', function ($data) {
                     return ($data->id);
                 })
-                ->addColumn('name', function ($data) {
+                ->addColumn('title', function ($data) {
+                    return ($data->name);
+                })
+                ->addColumn('title_fa', function ($data) {
                     return ($data->name);
                 })
                 ->addColumn('action', function ($data) {
@@ -62,12 +66,12 @@ class CustomertypeController extends Controller
     public function create()
     {
         $thispage       = [
-            'title'         => 'مدیریت نوع مشتریان',
-            'list_title'    => 'لیست نوع مشتریان',
-            'add_title'     => 'افزودن نوع مشتریان',
-            'create_title'  => 'ایجاد نوع مشتریان',
-            'edit_title'    => 'ویرایش نوع مشتریان',
-            'enter_title'   => 'ورود اطلاعات نوع مشتریان',
+            'title'         => 'مدیریت نوع کاربران',
+            'list_title'    => 'لیست نوع کاربران',
+            'add_title'     => 'افزودن نوع کاربران',
+            'create_title'  => 'ایجاد نوع کاربران',
+            'edit_title'    => 'ویرایش نوع کاربران',
+            'enter_title'   => 'ورود اطلاعات نوع کاربران',
         ];
         $menupanels     =   Menu_panel::whereStatus(4)->get();
         $submenupanels  =   Submenu_panel::whereStatus(4)->get();
@@ -79,9 +83,9 @@ class CustomertypeController extends Controller
     public function store(Request $request)
     {
         try{
-            $customertypes = new Customertype();
-            $customertypes->name        = $request->input('name');
-            $customertypes->user_id     = Auth::user()->id;
+            $customertypes = new TypeUser();
+            $customertypes->title        = $request->input('title');
+            $customertypes->title_fa     = $request->input('title_fa');
 
             $result = $customertypes->save();
 
@@ -113,14 +117,14 @@ class CustomertypeController extends Controller
     public function edit($id)
     {
         $thispage       = [
-            'title'         => 'مدیریت نوع مشتریان',
-            'list_title'    => 'لیست نوع مشتریان',
-            'add_title'     => 'افزودن نوع مشتریان',
-            'create_title'  => 'ایجاد نوع مشتریان',
-            'edit_title'    => 'ویرایش نوع مشتریان',
-            'enter_title'   => 'ورود اطلاعات نوع مشتریان',
+            'title'         => 'مدیریت نوع کاربران',
+            'list_title'    => 'لیست نوع کاربران',
+            'add_title'     => 'افزودن نوع کاربران',
+            'create_title'  => 'ایجاد نوع کاربران',
+            'edit_title'    => 'ویرایش نوع کاربران',
+            'enter_title'   => 'ورود اطلاعات نوع کاربران',
         ];
-        $customertypes  =   Customertype::whereId($id)->first();
+        $customertypes  =   TypeUser::whereId($id)->first();
         $menupanels     =   Menu_panel::whereStatus(4)->get();
         $submenupanels  =   Submenu_panel::whereStatus(4)->get();
 
@@ -132,8 +136,9 @@ class CustomertypeController extends Controller
     public function update(CustomertypeRequest $request)
     {
         try {
-                $customer = Customertype::findOrfail($request->input('customer_id'));
-                $customer->name                 = $request->input('name');
+                $customer = TypeUser::findOrfail($request->input('customer_id'));
+                $customer->title                 = $request->input('title');
+                $customer->title_fa              = $request->input('title_fa');
 
                 $result = $customer->save();
 
@@ -153,7 +158,7 @@ class CustomertypeController extends Controller
     {
         try {
 
-            $customertype = Customertype::findOrfail($request->input('id'));
+            $customertype = TypeUser::findOrfail($request->input('id'));
             $result1 = $customertype->delete();
             if ($result1 == true) {
                 $success = true;
