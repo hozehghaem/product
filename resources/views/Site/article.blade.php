57b4e757b9fce42059d08c2e75c0d0ef7a5dd016
amir <amirhpz2000@gmail.com>
2025-04-30 20:07:33 +0330

    v1.2.77 add article

--- resources/views/Site/article.blade.php
+++ resources/views/Site/article.blade.php
@@ -1,7 +1,6 @@
 @extends('master')
+
 @section('style')
-@endsection
-@section('main')
     <style>
         .table thead th {
             font-family: 'Vazirmatn RD FD', sans-serif;
@@ -14,6 +13,9 @@
             font-size: 14px;
         }
     </style>
+@endsection
+
+@section('main')
     <div class="page-title-area">
         <div class="container">
             <div class="page-title-content">
@@ -21,44 +23,8 @@
             </div>
         </div>
     </div>
+
     <div class="container my-5">
-
-        {{-- بخش پایان‌نامه‌ها --}}
-        <h4 class="text-center mb-4">پایان‌نامه‌ها</h4>
-        <div class="table-responsive">
-            <table class="table table-striped table-bordered">
-                <thead class="table-dark">
-                <tr>
-                    <th scope="col">عنوان</th>
-                    <th scope="col">نویسنده</th>
-                    <th scope="col">تاریخ انتشار</th>
-                    <th scope="col">خلاصه</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>بررسی تطبیقی مبانی فقهی و حقوقی معاملات</td>
-                    <td>محمد حسینی</td>
-                    <td>۱۴۰۱/۰۵/۱۰</td>
-                    <td>این پژوهش به بررسی مقایسه‌ای مبانی فقهی و حقوقی انواع معاملات پرداخته است...</td>
-                </tr>
-                <tr>
-                    <td>نقش اجتهاد در تکامل فقه شیعه</td>
-                    <td>زهرا احمدی</td>
-                    <td>۱۳۹۹/۰۳/۱۵</td>
-                    <td>تحلیل و بررسی اهمیت اجتهاد در تحولات فقه شیعه از دوران معاصر...</td>
-                </tr>
-                <tr>
-                    <td>فقه و حقوق تطبیقی در قوانین بین‌الملل</td>
-                    <td>علی اکبری</td>
-                    <td>۱۴۰۲/۰۱/۲۰</td>
-                    <td>این تحقیق بررسی مبانی حقوق تطبیقی در فقه و تأثیرات آن بر قوانین بین‌المللی را پوشش می‌دهد...</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-
-        {{-- بخش مقالات --}}
         <h4 class="text-center mt-5 mb-4">مقالات</h4>
         <div class="table-responsive">
             <table class="table table-striped table-bordered">
@@ -71,24 +37,16 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>اصول فقه اسلامی در عصر معاصر</td>
-                    <td>حسین عباسی</td>
-                    <td>۱۴۰۰/۰۸/۲۳</td>
-                    <td>تحلیلی بر کاربرد اصول فقه اسلامی در مسائل عصر حاضر و چالش‌های جدید آن...</td>
-                </tr>
-                <tr>
-                    <td>مطالعه تطبیقی حقوق زن در اسلام و قوانین مدرن</td>
-                    <td>نرگس رضایی</td>
-                    <td>۱۳۹۸/۱۲/۰۵</td>
-                    <td>این مقاله به بررسی و مقایسه حقوق زن در فقه اسلامی و دیدگاه‌های مدرن پرداخته است...</td>
-                </tr>
-                <tr>
-                    <td>تأثیر نظریه‌های فقهی بر قانون‌گذاری اسلامی</td>
-                    <td>مهدی موسوی</td>
-                    <td>۱۴۰۲/۰۲/۱۲</td>
-                    <td>در این مقاله نقش نظریه‌های فقهی در تدوین قوانین اسلامی بررسی می‌شود...</td>
-                </tr>
+                @foreach($posts as $post)
+                    @if($post->posttype == 15)
+                        <tr>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->author ?? '---' }}</td>
+                            <td>{{ \Morilog\Jalali\Jalalian::fromDateTime($post->created_at)->format('Y/m/d') }}</td>
+                            <td>{{ Str::limit(strip_tags($post->summary), 100) }}</td>
+                        </tr>
+                    @endif
+                @endforeach
                 </tbody>
             </table>
         </div>
